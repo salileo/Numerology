bfb168a6f54912e7da2e8faa00f9e3d258ab50c8
Salil Kapoor <salil.kapoor@gmail.com>
2019-05-08 13:39:10 -0700

    Initial upload

---
+++ NumerlogyCalculator/Calculator.cs
@@ -0,0 +1,596 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerlogyCalculator
+{
+    class Calculator
+    {
+        public bool AddingAcross { get; set; }
+
+        public int GetLifePathNumber(DateTime dateOfBirth, out bool karmicNumber)
+        {
+            if (AddingAcross)
+            {
+                return GetLifePathNumberAddingAcross(dateOfBirth, out karmicNumber);
+            }
+            else
+            {
+                return GetLifePathNumberReducingDown(dateOfBirth, out karmicNumber);
+            }
+        }
+
+        int GetLifePathNumberAddingAcross(DateTime dateOfBirth, out bool karmicNumber)
+        {
+            int sum = 0;
+            int[] digits = GetDigits(dateOfBirth.Day);
+            foreach (int digit in digits)
+            {
+                sum += digit;
+            }
+
+            digits = GetDigits(dateOfBirth.Month);
+            foreach (int digit in digits)
+            {
+                sum += digit;
+            }
+
+            digits = GetDigits(dateOfBirth.Year);
+            foreach (int digit in digits)
+            {
+                sum += digit;
+            }
+
+            karmicNumber = false;
+
+            while (sum >= 10)
+            {
+                karmicNumber = IsKarmicDebtNumber(sum);
+
+                if (sum == 11 || sum == 22 || sum == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    digits = GetDigits(sum);
+                    sum = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sum += digit;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        int GetLifePathNumberReducingDown(DateTime dateOfBirth, out bool karmicNumber)
+        {
+            int sumOfDay = dateOfBirth.Day;
+            while (sumOfDay >= 10)
+            {
+                if (sumOfDay == 11 || sumOfDay == 22 || sumOfDay == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    int[] digits = GetDigits(sumOfDay);
+                    sumOfDay = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sumOfDay += digit;
+                    }
+                }
+            }
+
+            int sumOfMonth = dateOfBirth.Month;
+            while (sumOfMonth >= 10)
+            {
+                if (sumOfMonth == 11 || sumOfMonth == 22 || sumOfMonth == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    int[] digits = GetDigits(sumOfMonth);
+                    sumOfMonth = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sumOfMonth += digit;
+                    }
+                }
+            }
+
+            int sumOfYear = dateOfBirth.Year;
+            while (sumOfYear >= 10)
+            {
+                if (sumOfYear == 11 || sumOfYear == 22 || sumOfYear == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    int[] digits = GetDigits(sumOfYear);
+                    sumOfYear = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sumOfYear += digit;
+                    }
+                }
+            }
+
+            karmicNumber = false;
+
+            int sum = sumOfDay + sumOfMonth + sumOfYear;
+            while (sum >= 10)
+            {
+                karmicNumber = IsKarmicDebtNumber(sum);
+
+                if (sum == 11 || sum == 22 || sum == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    int[] digits = GetDigits(sum);
+                    sum = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sum += digit;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        public int GetDestinyNumber(string nameAtBirth, out bool karmicNumber)
+        {
+            int sum = 0;
+            string[] parts = nameAtBirth.Split(new char[] { ' ' });
+            foreach (string part in parts)
+            {
+                int sumOfPart = 0;
+                int[] digits = GetDigits(part);
+                foreach (int digit in digits)
+                {
+                    sumOfPart += digit;
+                }
+
+                while (sumOfPart >= 10)
+                {
+                    if (sumOfPart == 11 || sumOfPart == 22 || sumOfPart == 33)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        digits = GetDigits(sumOfPart);
+                        sumOfPart = 0;
+
+                        foreach (int digit in digits)
+                        {
+                            sumOfPart += digit;
+                        }
+                    }
+                }
+
+                sum += sumOfPart;
+            }
+
+            karmicNumber = false;
+
+            while (sum >= 10)
+            {
+                karmicNumber = IsKarmicDebtNumber(sum);
+
+                if (sum == 11 || sum == 22 || sum == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    int[] digits = GetDigits(sum);
+                    sum = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sum += digit;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        public int GetSoulNumber(string nameAtBirth, out bool karmicNumber)
+        {
+            int sum = 0;
+            string[] parts = nameAtBirth.Split(new char[] { ' ' });
+            foreach (string part in parts)
+            {
+                int sumOfPart = 0;
+                char[] vowels = GetVowels(part);
+                foreach (char vowel in vowels)
+                {
+                    sumOfPart += GetDigitForAlphabet(vowel);
+                }
+
+                while (sumOfPart >= 10)
+                {
+                    if (sumOfPart == 11 || sumOfPart == 22 || sumOfPart == 33)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        int[] digits = GetDigits(sumOfPart);
+                        sumOfPart = 0;
+
+                        foreach (int digit in digits)
+                        {
+                            sumOfPart += digit;
+                        }
+                    }
+                }
+
+                sum += sumOfPart;
+            }
+
+            karmicNumber = false;
+
+            while (sum >= 10)
+            {
+                karmicNumber = IsKarmicDebtNumber(sum);
+
+                if (sum == 11 || sum == 22 || sum == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    int[] digits = GetDigits(sum);
+                    sum = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sum += digit;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        public int GetPersonalityNumber(string nameAtBirth, out bool karmicNumber)
+        {
+            int sum = 0;
+            string[] parts = nameAtBirth.Split(new char[] { ' ' });
+            foreach (string part in parts)
+            {
+                int sumOfPart = 0;
+                char[] consonents = GetConsonents(part);
+                foreach (char consonent in consonents)
+                {
+                    sumOfPart += GetDigitForAlphabet(consonent);
+                }
+
+                while (sumOfPart >= 10)
+                {
+                    if (sumOfPart == 11 || sumOfPart == 22 || sumOfPart == 33)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        int[] digits = GetDigits(sumOfPart);
+                        sumOfPart = 0;
+
+                        foreach (int digit in digits)
+                        {
+                            sumOfPart += digit;
+                        }
+                    }
+                }
+
+                sum += sumOfPart;
+            }
+
+            karmicNumber = false;
+
+            while (sum >= 10)
+            {
+                karmicNumber = IsKarmicDebtNumber(sum);
+
+                if (sum == 11 || sum == 22 || sum == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    int[] digits = GetDigits(sum);
+                    sum = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sum += digit;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        public int GetMaturityNumber(DateTime dateOfBirth, string nameAtBirth, out bool karmicNumber)
+        {
+            int sum = GetLifePathNumber(dateOfBirth, out karmicNumber) + GetDestinyNumber(nameAtBirth, out karmicNumber);
+
+            karmicNumber = false;
+
+            while (sum >= 10)
+            {
+                karmicNumber = IsKarmicDebtNumber(sum);
+
+                if (sum == 11 || sum == 22 || sum == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    int[] digits = GetDigits(sum);
+                    sum = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sum += digit;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        public int GetCurrentNameNumber(string currentName, out bool karmicNumber)
+        {
+            int sum = 0;
+            string[] parts = currentName.Split(new char[] { ' ' });
+            foreach (string part in parts)
+            {
+                int sumOfPart = 0;
+                int[] digits = GetDigits(part);
+                foreach (int digit in digits)
+                {
+                    sumOfPart += digit;
+                }
+
+                while (sumOfPart >= 10)
+                {
+                    if (sumOfPart == 11 || sumOfPart == 22 || sumOfPart == 33)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        digits = GetDigits(sumOfPart);
+                        sumOfPart = 0;
+
+                        foreach (int digit in digits)
+                        {
+                            sumOfPart += digit;
+                        }
+                    }
+                }
+
+                sum += sumOfPart;
+            }
+
+            karmicNumber = false;
+
+            while (sum >= 10)
+            {
+                karmicNumber = IsKarmicDebtNumber(sum);
+
+                if (sum == 11 || sum == 22 || sum == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    int[] digits = GetDigits(sum);
+                    sum = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sum += digit;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        public int GetBirthDayNumber(DateTime dateOfBirth, out bool karmicNumber)
+        {
+            int sumOfDay = dateOfBirth.Day;
+
+            karmicNumber = false;
+
+            while (sumOfDay >= 10)
+            {
+                karmicNumber = IsKarmicDebtNumber(sumOfDay);
+
+                if (sumOfDay == 11 || sumOfDay == 22 || sumOfDay == 33)
+                {
+                    break;
+                }
+                else
+                {
+                    int[] digits = GetDigits(sumOfDay);
+                    sumOfDay = 0;
+
+                    foreach (int digit in digits)
+                    {
+                        sumOfDay += digit;
+                    }
+                }
+            }
+
+            return sumOfDay;
+        }
+
+        public List<int> GetKarmicLessonNumber(string nameAtBirth)
+        {
+            List<int> result = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (char alphabet in nameAtBirth)
+            {
+                int number = GetDigitForAlphabet(alphabet);
+                result.Remove(number);
+            }
+
+            return result;
+        }
+
+        bool IsKarmicDebtNumber(int number)
+        {
+            switch (number)
+            {
+                case 13:
+                case 14:
+                case 16:
+                case 19:
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        int[] GetDigits(int number)
+        {
+            List<int> digits = new List<int>();
+            while (number > 0)
+            {
+                int digit = number % 10;
+                number = (number - digit) / 10;
+
+                if (digit != 0)
+                {
+                    digits.Add(digit);
+                }
+            }
+
+            return digits.ToArray();
+        }
+
+        int[] GetDigits(string name)
+        {
+            List<int> digits = new List<int>();
+            foreach (char alphabet in name)
+            {
+                int number = GetDigitForAlphabet(alphabet);
+                if (number > 0)
+                {
+                    digits.Add(number);
+                }
+            }
+
+            return digits.ToArray();
+        }
+
+        char[] GetVowels(string name)
+        {
+            List<char> vowels = new List<char>();
+            for (int i = 0; i < name.Length; i++)
+            {
+                if ((i == 0 && name[i] == 'Y') &&
+                    ((i + 1) < name.Length && !IsVowel(name[i + 1])))
+                {
+                    vowels.Add(name[i]);
+                }
+                else if ((i == name.Length - 1 && name[i] == 'Y') &&
+                         ((i - 1) >= 0 && !IsVowel(name[i - 1])))
+                {
+                    vowels.Add(name[i]);
+                }
+                else if ((name[i] == 'Y') &&
+                         ((i - 1) >= 0 && !IsVowel(name[i - 1])) &&
+                         ((i + 1) < name.Length && !IsVowel(name[i + 1])))
+                {
+                    vowels.Add(name[i]);
+                }
+                else if (IsVowel(name[i]))
+                {
+                    vowels.Add(name[i]);
+                }
+            }
+
+            return vowels.ToArray();
+        }
+
+        char[] GetConsonents(string name)
+        {
+            List<char> consonents = new List<char>();
+            for (int i = 0; i < name.Length; i++)
+            {
+                if ((i == 0 && name[i] == 'Y') &&
+                    ((i + 1) < name.Length && IsVowel(name[i + 1])))
+                {
+                    consonents.Add(name[i]);
+                }
+                else if ((i == name.Length - 1 && name[i] == 'Y') &&
+                         ((i - 1) >= 0 && IsVowel(name[i - 1])))
+                {
+                    consonents.Add(name[i]);
+                }
+                else if ((name[i] == 'Y') &&
+                         (((i - 1) >= 0 && IsVowel(name[i - 1])) ||
+                          ((i + 1) < name.Length && IsVowel(name[i + 1]))))
+                {
+                    consonents.Add(name[i]);
+                }
+                else if (!IsVowel(name[i]))
+                {
+                    consonents.Add(name[i]);
+                }
+            }
+
+            return consonents.ToArray();
+        }
+
+        bool IsVowel(char alphabet)
+        {
+            switch (alphabet)
+            {
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        int GetDigitForAlphabet(char alphabet)
+        {
+            if (char.IsLetter(alphabet))
+            {
+                int charValue = alphabet - (int)'A';
+                return (charValue % 9) + 1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ NumerlogyCalculator/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumerlogyCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 1; i <= 31; i++)
+            {
+                this.DOB_Day.Items.Add(i);
+            }
+
+            this.NameAtBirth.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Result.Text = string.Empty;
+
+            string nameAtBirth = this.NameAtBirth.Text.ToUpper().Trim();
+            if (string.IsNullOrWhiteSpace(nameAtBirth))
+            {
+                MessageBox.Show("Please provide the name at birth.");
+                return;
+            }
+
+            string currentName = this.CurrentName.Text.ToUpper().Trim();
+            if (string.IsNullOrWhiteSpace(currentName))
+            {
+                MessageBox.Show("Please provide the current name.");
+                return;
+            }
+
+            DateTime dateOfBirth;
+            try
+            {
+                dateOfBirth = new DateTime(int.Parse(this.DOB_Year.Text), this.DOB_Month.SelectedIndex + 1, this.DOB_Day.SelectedIndex + 1);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please provide a valid date of birth.");
+                return;
+            }
+
+            Calculator calculator = new Calculator();
+            StringBuilder bldr = new StringBuilder();
+            bldr.AppendLine(string.Format("Name at birth : {0}", nameAtBirth));
+            bldr.AppendLine(string.Format("Current name : {0}", currentName));
+            bldr.AppendLine(string.Format("Date of birth : {0}", dateOfBirth.ToLongDateString()));
+            bldr.AppendLine();
+            bldr.AppendLine("Numbers :");
+
+            const string karmicNumberString = " -- [Karmic Debt Number]";
+            int number;
+            bool karmicNumber;
+
+            number = calculator.GetLifePathNumber(dateOfBirth, out karmicNumber);
+            bldr.AppendLine(string.Format("Life Path Number = {0}{1}", number, karmicNumber ? karmicNumberString : ""));
+
+            number = calculator.GetDestinyNumber(nameAtBirth, out karmicNumber);
+            bldr.AppendLine(string.Format("Destiny Number = {0}{1}", number, karmicNumber ? karmicNumberString : ""));
+
+            number = calculator.GetSoulNumber(nameAtBirth, out karmicNumber);
+            bldr.AppendLine(string.Format("Soul Number = {0}{1}", number, karmicNumber ? karmicNumberString : ""));
+
+            number = calculator.GetPersonalityNumber(nameAtBirth, out karmicNumber);
+            bldr.AppendLine(string.Format("Personality Number = {0}{1}", number, karmicNumber ? karmicNumberString : ""));
+
+            number = calculator.GetMaturityNumber(dateOfBirth, nameAtBirth, out karmicNumber);
+            bldr.AppendLine(string.Format("Maturity Number = {0}{1}", number, karmicNumber ? karmicNumberString : ""));
+
+            number = calculator.GetCurrentNameNumber(currentName, out karmicNumber);
+            bldr.AppendLine(string.Format("Current Name Number = {0}{1}", number, karmicNumber ? karmicNumberString : ""));
+
+            number = calculator.GetBirthDayNumber(dateOfBirth, out karmicNumber);
+            bldr.AppendLine(string.Format("Birth Day Number = {0}{1}", number, karmicNumber ? karmicNumberString : ""));
+
+            string karmicLessonNumber = string.Empty;
+            List<int> karmicLessonNumbers = calculator.GetKarmicLessonNumber(nameAtBirth);
+            foreach (int num in karmicLessonNumbers)
+            {
+                if (!string.IsNullOrEmpty(karmicLessonNumber))
+                {
+                    karmicLessonNumber += ", ";
+                }
+
+                karmicLessonNumber += num.ToString();
+            }
+
+            bldr.AppendLine(string.Format("Karmic Lesson Numbers = {0}", karmicLessonNumber));
+
+            this.Result.Text = bldr.ToString();
+        }
+    }
+}
